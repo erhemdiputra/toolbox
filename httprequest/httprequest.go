5c7c3b1bfe981a503bf8f681cf6664d886ade9d8
erhemdiputra <erhem.diputra@tokopedia.com>
2019-08-12 15:35:04 +0700

    add method post and get, update code :zap:

--- httprequest/httprequest.go
+++ httprequest/httprequest.go
@@ -1,15 +1,22 @@
 package httprequest
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"time"
 )
 
 var (
 	ErrStatusCodeNotOk = errors.New("status code not ok")
+)
+
+const (
+	ContentTypeApplicationJSON = "application/json"
+	ContentTypeFormURLEncoded  = "application/x-www-form-urlencoded"
 )
 
 type HTTPRequest struct {
@@ -24,12 +31,59 @@ func NewHTTPRequest(timeout time.Duration) *HTTPRequest {
 	}
 }
 
-func (h *HTTPRequest) DoRequest(method, url string, out interface{}) error {
-	request, err := http.NewRequest(method, url, nil)
-	if err != nil {
-		return err
+func parseToURLValues(param map[string]string) url.Values {
+	queryString := make(url.Values)
+
+	for key, value := range param {
+		queryString.Add(key, value)
 	}
 
+	return queryString
+}
+
+func (h *HTTPRequest) Get(URL string, header http.Header, param map[string]string, out interface{}) error {
+	urlValues := parseToURLValues(param)
+
+	request, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		return errors.New("failed to create get request")
+	}
+
+	request.Header = header
+	request.URL.RawQuery = urlValues.Encode()
+
+	return h.DoRequest(request, out)
+}
+
+func (h *HTTPRequest) Post(URL string, header http.Header, param map[string]string, payload interface{}, out interface{}) error {
+	var paramBuffer *bytes.Buffer
+	var err error
+
+	if payload != nil {
+		header.Set("Content-Type", ContentTypeApplicationJSON)
+		marshalled, err := json.Marshal(payload)
+		if err != nil {
+			return errors.New("failed to marshal payload")
+		}
+		paramBuffer = bytes.NewBuffer(marshalled)
+
+	} else {
+		header.Set("Content-Type", ContentTypeFormURLEncoded)
+		queryStringEncoded := parseToURLValues(param).Encode()
+		paramBuffer = bytes.NewBufferString(queryStringEncoded)
+	}
+
+	request, err := http.NewRequest("POST", URL, paramBuffer)
+	if err != nil {
+		return errors.New("failed to create post request")
+	}
+
+	request.Header = header
+
+	return h.DoRequest(request, out)
+}
+
+func (h *HTTPRequest) DoRequest(request *http.Request, out interface{}) error {
 	response, err := h.Client.Do(request)
 	if err != nil {
 		return err
@@ -42,7 +96,8 @@ func (h *HTTPRequest) DoRequest(method, url string, out interface{}) error {
 		return err
 	}
 
-	if response.StatusCode != http.StatusOK {
+	if response.StatusCode != http.StatusOK && response.StatusCode != http.StatusCreated &&
+		response.StatusCode != http.StatusNoContent {
 		return ErrStatusCodeNotOk
 	}
 
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/erhemdiputra/toolbox/httprequest"
@@ -17,19 +18,37 @@ func printSeparator() {
 
 func tryHTTPRequestPackage() {
 	log.Println("[tryHTTPRequestPackage]")
-
-	type Data struct {
-		UserID    int64  `json:"userId"`
-		ID        int64  `json:"id"`
-		Title     string `json:"title"`
-		Completed bool   `json:"completed"`
-	}
-
-	var data Data
-
 	httpRequestInst := httprequest.NewHTTPRequest(5 * time.Second)
-	err := httpRequestInst.DoRequest("GET", "https://jsonplaceholder.typicode.com/todos/1", &data)
-	log.Printf("Data: %+v, Error: %+v\n", data, err)
+
+	// GET
+	getResp := make(map[string]interface{})
+
+	err := httpRequestInst.Get("https://jsonplaceholder.typicode.com/todos/1", http.Header{}, map[string]string{
+		"hello":    "world",
+		"kenpachi": "zaraki",
+	}, &getResp)
+	log.Printf("[GET] Data: %+v, Error: %+v\n", getResp, err)
+	// EOF GET
+
+	// POST
+	postResp := make(map[string]interface{})
+
+	err = httpRequestInst.Post("https://jsonplaceholder.typicode.com/posts",
+		http.Header{}, map[string]string{
+			"shinigami": "ichigo kurosaki, byakuya kuchiki, renji abarai!",
+		}, nil, &postResp,
+	)
+	log.Printf("[POST][Form URL Encoded] Data: %+v, Error: %+v\n", postResp, err)
+
+	err = httpRequestInst.Post("https://jsonplaceholder.typicode.com/posts",
+		http.Header{}, nil, map[string]interface{}{
+			"title":  "foo",
+			"body":   "bar",
+			"userId": 1,
+		}, &postResp,
+	)
+	log.Printf("[POST][JSON] Data: %+v, Error: %+v\n", postResp, err)
+	// EOF POST
 
 	printSeparator()
 }
