16b7bb813aa6d6a50c6d6a12210cd9ccccfe0b69
erhemdiputra <erhem.diputra@tokopedia.com>
2019-08-12 18:58:02 +0700

    update circuit breaker pkg

--- circuitbreaker/circuitbreaker.go
+++ circuitbreaker/circuitbreaker.go
@@ -2,28 +2,11 @@ package circuitbreaker
 
 import (
 	"errors"
-	"sync"
 
 	circuit "github.com/rubyist/circuitbreaker"
 )
 
-var (
-	mapBreaker = make(map[string]*circuit.Breaker)
-	mutex      = &sync.Mutex{}
-)
-
-// call this function in init()
-func SetupMapBreaker(maps map[string]*circuit.Breaker) {
-	mapBreaker = maps
-}
-
-func GetCircuitBreaker(name string) *circuit.Breaker {
-	return mapBreaker[name]
-}
-
-func CircuitBreakerWrap(name string, processFunc func() error) error {
-	breaker := GetCircuitBreaker(name)
-
+func CircuitBreakerWrap(breaker *circuit.Breaker, processFunc func() error) error {
 	if breaker.Ready() {
 		err := processFunc()
 		if err != nil {
